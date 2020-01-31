e0750b8dda3b98332940c504da63912564f6ccdf
venum966 <60420622+venum966@users.noreply.github.com>
2020-01-31 10:54:10 +0530

    Add files via upload

---
+++ sample.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+ {
+   int fact=1,i,n=5;
+   int temp=n;
+   while(temp>0)
+    {
+      fact=fact*temp;
+	temp;
+    }
+  printf("factorial of %d is %d\n",n,fact);
+ }
